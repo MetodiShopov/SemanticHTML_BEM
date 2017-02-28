3499d543b8a9f995c8c5123961769fc3cc3f8d22
Metodi Shopov <shopov.metodi@gmail.com>
2017-02-28 22:48:30 +0200

    HTML:  - removed onclick functions  - swapped article with section tag as more appropriate  - swapped aside section with details tag as more appropriate
     CSS:
     - changes according to above mentioned tag changes
     - edited details functionality
    
     JS:
     - removed window.onclick function
     - changed all functions to be invoked .onload
     - functions are now scalable

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,18 @@
-window.onload = function dropDown(){
+window.onload = function () {
     "use strict";
-    let allDropDowns = document.getElementsByClassName("dropDown");
 
-    for (let i = 0; i < allDropDowns.length; i++) {
-        allDropDowns[i].onclick = function(){
-            this.classList.toggle("active");
+    // Navigation DropDown function
+    let allDropDowns = document.querySelectorAll(".dropDown");
+    allDropDowns.forEach(drops => drops.addEventListener('click', function(){
+        this.classList.toggle("active");
+        this.childNodes[1].classList.toggle('show');
+    }));
 
-            if(this.classList.contains('accordion')){
-                this.nextElementSibling.classList.toggle("show");
-                if(this.classList.contains('active')){
-                    this.innerText = 'Click to close';
-                } else {
-                    this.innerText = 'Click to open';
-                }
-            } else {
-                this.childNodes[1].classList.toggle('show');
-            }
-        }
-    }
+    //Language selection function
+    let allLanguageFlags = document.querySelectorAll('.nav__language > img');
+    allLanguageFlags.forEach(element => element.addEventListener('click', function () {
+            allLanguageFlags.forEach(img => img.classList.remove('selected'));
+            this.classList.add('selected');
+        })
+    );
 };
-
-window.onclick = function(e) {
-    if (!e.target.matches('.dropDown')) {
-
-        let dropdowns = document.getElementsByClassName("dropDown__content");
-        for (let i = 0; i < dropdowns.length; i++) {
-            let openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-};
-
-function selectLang(id){
-    "use strict";
-    let languages = document.getElementById('lang-menu').getElementsByTagName('li');
-    for (let i = 0; i < languages.length; i++) {
-        languages[i].childNodes[0].style.opacity = '0.3';
-    }
-    document.getElementById(id).style.opacity = '1';
-}
